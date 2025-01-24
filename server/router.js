15be0f2117f065d3c22bd015433bb68104a9949a
Mark Grimke <MarsAltar01@gmail.com>
2025-01-23 19:56:26 -0500

    got rid of secure requirement for map

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/map', mid.requiresSecure, controllers.Shroomcraft.mapPage)
+  app.get('/map', controllers.Shroomcraft.mapPage)
 
   app.get('/', mid.requiresSecure, controllers.Shroomcraft.homePage);
   app.get('/*', controllers.get404);
